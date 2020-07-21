1372c89ce09c460db6698863495d178b673a20f7
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-21 15:14:12 +0100

    a

--- _tobin.c
+++ _tobin.c
@@ -11,20 +11,20 @@
 */
 int _tobin(unsigned int d)
 {
-    int i, j = 0;
-    char *s;
+int i, j = 0;
+char *s;
 
-    s = malloc(sizeof(char)*8);
-    for (i = 0; d / 2 != 0; ++i)
-    {
-        s[i] = d % 2 + '0';
-        d /= 2;
-        j++;
-    }
-    s[i] = d % 2 + '0';
-    for (; i >= 0; --i)
-    {
-        _putchar(s[i]);
-    }
-    return (j + 1);
+s = malloc(sizeof(char) * 8);
+for (i = 0; d / 2 != 0; ++i)
+{
+s[i] = d % 2 + '0';
+d /= 2;
+j++;
+}
+s[i] = d % 2 + '0';
+for (; i >= 0; --i)
+{
+_putchar(s[i]);
+}
+return (j + 1);
 }
