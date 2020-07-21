12da92bc918fb1fe1f6e9af660d004caab2b97c8
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-07-21 10:17:41 +0100

    morning

--- _printf.c
+++ _printf.c
@@ -44,25 +44,17 @@ void print_str(char *str)
 	}
 }
 /**
-*_printf - 0
-*@s: char
+*cases - 0
+*@ap: list of arguments
+*@s: string
+*@i: position of char
+*@r: num written chars
 *Return: length
 */
-int _printf(const char *s, ...)
+void cases(va_list ap, const char *s, int i, int r)
 {
-	int i, l = 0, r = 0;
 	char *ch;
-	va_list ap;
-
-	l = _strlen(s);
-	va_start(ap, 0);
-	for (i = 0; i < l; i++)
-	{
-		if (s[i] != '%' || (s[i] == '%' && s[i + 1] == '\0'))
-			_putchar(s[i]);
-		else
-		{
-			switch (s[i + 1])
+				switch (s[i + 1])
 			{
 				case 'c':
 					_putchar(va_arg(ap, int));
@@ -85,6 +77,25 @@ int _printf(const char *s, ...)
 					_putchar(s[i]);
 					break;
 			}
+}
+/**
+*_printf - 0
+*@s: char
+*Return: length
+*/
+int _printf(const char *s, ...)
+{
+	int i, l = _strlen(s), r = 0;
+	va_list ap;
+
+	va_start(ap, 0);
+	for (i = 0; i < l; i++)
+	{
+		if (s[i] != '%' || (s[i] == '%' && s[i + 1] == '\0'))
+			_putchar(s[i]);
+		else
+		{
+			cases(ap, s, i, r);
 		}
 	}
 	r += l;
